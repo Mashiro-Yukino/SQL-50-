c40938e48f658582109d8a1b7197ee07f7d9b4a6
Mashiro-Yukino <67991315+Mashiro-Yukino@users.noreply.github.com>
2020-12-20 13:10:18 +0800

    Update SQL经典50题.sql

--- SQL经典50题.sql
+++ SQL经典50题.sql
@@ -1,4 +1,4 @@
- #SC(SId,CId,score) SId 学生编号,CId 课程编号,score#
+#SC(SId,CId,score) SId 学生编号,CId 课程编号,score#
 create table Student(SId varchar(10),Sname varchar(10),Sage datetime,Ssex varchar(10));
 insert into Student values('01' , '赵雷' , '1990-01-01' , '男');
 insert into Student values('02' , '钱电' , '1990-12-21' , '男');
@@ -54,7 +54,6 @@ insert into SC values('07' , '03' , 98);
 
 
 #12.检索" 01 "课程分数小于 60，按分数降序排列的学生信息#
-
 SELECT * FROM Student JOIN SC ON Student.SId=SC.SId
 WHERE CId = 01 AND score < 60
 ORDER BY score DESC
@@ -67,7 +66,6 @@ ORDER BY score DESC
 及格为>=60，中等为：70-80，优良为：80-90，优秀为：>=90
 要求输出课程号和选修人数，查询结果按人数降序排列列，若人数相同，按课程号升序
 排列#
-
 select cid,count(sid), max(score), min(score), avg(score),
 sum(pass)/count(sid) as passrate, sum(middle)/count(sid) as middlerate,
 sum(good)/count(sid) as goodrate, sum(great)/count(sid) as greatrate
@@ -81,27 +79,23 @@ from sc)a group by cid order by count(sid) desc, cid
 
 
 #15.按各科成绩进行排序，并显示排名， Score 重复时保留名次空缺#
-
 select sid, cid, score, 
 dense_rank()over (partition by cid
 order by score desc) as ranking from sc 
 
 
 #16.查询学生的总成绩，并进行排名，总分重复时保留名次空缺#
-
 select sid, sum(score),
 rank()over (order by sum(score) desc) as ranking
 from sc group by sid
 
 
 #24.查询 1990 年年出生的学生名单#
-
 select student.* from student
 where sage like '1990%'
 
 
 #33.成绩不重复，查询选修「张三」老师所授课程的学生中，成绩最高的学生信息及其成绩#
-
 select student.*, score from sc
 left join student on sc.sid = student.sid
 left join course on sc.cid = course.cid

0c448e404224b1207ec0eafab3422371dae8cae5
Mashiro-Yukino <67991315+Mashiro-Yukino@users.noreply.github.com>
2020-12-23 21:17:31 +0800

    Update and rename 牛客网 to 牛客网.sql

---
+++ SQL/牛客网.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));

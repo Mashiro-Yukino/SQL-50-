01cc0c3f26e94333ac94b99525e0eb9f5c0619ff
Mashiro-Yukino <67991315+Mashiro-Yukino@users.noreply.github.com>
2020-12-23 21:53:42 +0800

    Update 牛客网.sql

--- SQL/牛客网.sql
+++ SQL/牛客网.sql
@@ -6,3 +6,46 @@ CREATE TABLE `employees` (
 `gender` char(1) NOT NULL,
 `hire_date` date NOT NULL,
 PRIMARY KEY (`emp_no`));
+             
+
+             
+https://www.nowcoder.com/practice/ec1ca44c62c14ceb990c3c40def1ec6c?tpId=82&&tqId=29754&rp=1&ru=/ta/sql&qru=/ta/sql/question-ranking             
+#1.查找入职员工时间排名倒数第三的员工所有信息，为了减轻入门难度，目前所有的数据里员工入职的日期都不是同一天#  
+             
+SELECT * FROM employees
+ORDER BY hire_date DESC
+LIMIT 1 offset 2
+
+#当limit和offset组合使用的时候，limit后面只能有一个参数，
+#表示要取的的数量,offset表示要跳过的数量 。      
+             
+             
+        
+             
+https://www.nowcoder.com/practice/c63c5b54d86e4c6d880e4834bfd70c3b?tpId=82&tags=&title=&diffculty=0&judgeStatus=0&rp=1             
+2.查找各个部门当前(dept_manager.to_date='9999-01-01')领导当前(salaries.to_date='9999-01-01')薪水详情以及其对应部门编号dept_no
+(注:输出结果以salaries.emp_no升序排序，并且请注意输出结果里面dept_no列是最后一列)
+CREATE TABLE `salaries` (
+`emp_no` int(11) NOT NULL, -- '员工编号',
+`salary` int(11) NOT NULL,
+`from_date` date NOT NULL,
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`from_date`));
+CREATE TABLE `dept_manager` (
+`dept_no` char(4) NOT NULL, -- '部门编号'
+`emp_no` int(11) NOT NULL, --  '员工编号'
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`dept_no`));             
+             
+             
+select s.*,d.dept_no
+from salaries as s  inner join dept_manager as d
+on s.emp_no=d.emp_no
+where s.to_date='9999-01-01' and d.to_date='9999-01-01'             
+             
+             
+             
+             
+             
+                     
+             

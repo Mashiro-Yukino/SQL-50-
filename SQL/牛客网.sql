62438cb3c956f2b06e137c9ff458d96affd83255
Mashiro-Yukino <67991315+Mashiro-Yukino@users.noreply.github.com>
2020-12-24 19:59:57 +0800

    Update 牛客网.sql

--- SQL/牛客网.sql
+++ SQL/牛客网.sql
@@ -123,3 +123,23 @@ FROM titles
 GROUP BY title
 HAVING t>=2;             
              
+             
+             
+             
+             
+https://www.nowcoder.com/practice/a32669eb1d1740e785f105fa22741d5c?tpId=82&tags=&title=&diffculty=0&judgeStatus=0&rp=1
+6.查找employees表所有emp_no为奇数，且last_name不为Mary(注意大小写)的员工信息，并按照hire_date逆序排列
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));
+             
+select *
+FROM employees
+WHERE emp_no % 2 = 1
+AND last_name<>'Mary'
+ORDER BY hire_date DESC;             
